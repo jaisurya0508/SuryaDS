fe5678b14fc9784933c94a9c52bd0553368533df
jaisurya0508 <67913156+jaisurya0508@users.noreply.github.com>
2025-01-16 21:29:37 +0530

    Create loc.py

---
+++ loc.py
@@ -0,0 +1,4 @@
+•	Experian data for rejects and NTUs: s3://bmf-analytics-dev-eu-west-2-leobrix/data/Retros/September2024Retros/Experian/RawData/CLIENT_FILE.csv
+•	TU data for rejects and NTUs: s3://bmf-analytics-dev-eu-west-2-leobrix/data/Retros/September2024Retros/TransUnion/RawData/TrueVisionData_RejectsNTUs.csv
+•	The accept/reject flag (note that this also contains a "refer" flag for cases that are referred to our underwriting team for manual review - not sure how you handle these, Hindol?): s3://bmf-analytics-dev-eu-west-2-leobrix/data/Retros/September2024Retros/Experian/RawData/BMF_Retro_08_2024.csv
+•	A dataset with the non-credit variables for every application: s3://bmf-analytics-dev-eu-west-2-leobrix/data/Retros/September2024Retros/NonCreditVariables.csv
